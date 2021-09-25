7be6317e9cc43afee5d898f119ba96cade26d247
Arkadiusz Piekarz <piekarzarkadiusz@gmail.com>
2021-09-25 17:34:44 +0200

    Strip output binary in build-release.sh
    This reduces the size of the binary from 2.5 MB to 1.2 MB.

--- build-release.sh
+++ build-release.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Script for building Rusty Tax Break with options tuned for better runtime performance on stable Rust.
+# Script for building Rusty Tax Break with options tuned for better runtime performance and smaller binary.
 # It can increase the compilation time in comparison to default release profile.
 
-RUSTFLAGS="-Clink-arg=-fuse-ld=lld -Ctarget-cpu=native" cargo build --release
+RUSTFLAGS="-Clink-arg=-fuse-ld=lld -Ctarget-cpu=native" cargo build --release && strip target/release/rusty-tax-break
